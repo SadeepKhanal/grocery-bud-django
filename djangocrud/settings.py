b92d6422033db4b4389ccd5c385d51ebe721ff53
SadeepKhanal <sadeepkhanal343@gmail.com>
2026-02-24 22:54:01 +0545

    deployment fix

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -117,5 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",  # optional if you have project-level static
+# ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "static"]
