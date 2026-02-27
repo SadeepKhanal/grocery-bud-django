2da33200e50fd3ae43aa1a66b70ee80c1290e439
SadeepKhanal <sadeepkhanal343@gmail.com>
2026-02-27 12:34:49 +0545

    deployment

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(0w#6ors_*&f^&!)xh!^x+cyp1@8aj3duzde=&!2ff(ixtjew4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG ='RENDER' not in os.environ
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ["grocery-bud-django.onrender.com","127.0.0.1", "localhost","grocery.onrender.com","grocery-bud.onrender.com"]
+ALLOWED_HOSTS = ["grocery-bud-django.onrender.com", "127.0.0.1",
+                 "localhost", "grocery.onrender.com", "grocery-bud.onrender.com"]
 
 
 # Application definition
@@ -117,9 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-
-
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = "/static/"
 if not DEBUG:
